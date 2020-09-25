277619a856453f5a58d0caa986ace889158607f1
xiaoyao58 <tianyixia58@gamil.com>
2020-09-25 14:57:50 +0800

    update router.go

--- router/router.go
+++ router/router.go
@@ -8,10 +8,12 @@ import (
 
 func init() {
 	router := gin.Default()
+	router.Static("/static/avatar","./static/avatar")
+	router.Static("/static/image","./static/image")
 	router.POST("/blog/register", func(ctx *gin.Context) { controller.Register(ctx) })
 	router.POST("/blog/login", func(ctx *gin.Context) { controller.Login(ctx) })
 	router.POST("/blog/sendmail", func(ctx *gin.Context) { controller.SendMail(ctx) })
-	router.GET("/user/getAll", middleware.TokenParse(), func(ctx *gin.Context) { controller.GetAllUser(ctx) })
+	router.GET("/blog/user/getAll", middleware.TokenParse(), func(ctx *gin.Context) { controller.GetAllUser(ctx) })
 	//router.GET("/user/getMine",middleware.TokenParse(),func(ctx *gin.Context){controller.GetMine(ctx)})
 	router.Run("localhost:8080")
 }
